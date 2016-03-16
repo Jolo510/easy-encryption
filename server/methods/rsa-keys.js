6e297ccc8fe7339c1c12842b2bf22da56400e7a1
Johnnie Lo <johnnielo50@gmail.com>
2016-03-15 23:56:21 -0700

    Changed console.logs to display detail information.

--- server/methods/rsa-keys.js
+++ server/methods/rsa-keys.js
@@ -5,19 +5,17 @@ Meteor.methods({
 			urlId: token
 		});
 
-		console.log("Token : " + user);
-
 		if ( user ) {
 			if (user.isPrivateKeyDownloaded) {
 				// Already downloaded private key
-				console.log("User already downloaded private key.");
+				console.log("Token : " + token + " User already downloaded private key.");
 				return {
 					isValidLink: true,
 					downloadKey: false
 				}
 			} else {
 				// Private hasn't been downloaded. User downloads private key, then save public key
-				console.log("Private key hasn't been downloaded yet.");
+				console.log("Token : " + token + " Private key hasn't been downloaded yet.");
 				return {
 					isValidLink: true,
 					downloadKey: true,
@@ -26,7 +24,7 @@ Meteor.methods({
 			}
 		} else {
 			// No user found. No urlId found. Invalid link
-			console.log("Not a valid url link.");
+			console.log("Token : " + token + " Not a valid url link.");
 			return { 
 				isValidLink: false,
 				downloadKey: false
@@ -34,6 +32,18 @@ Meteor.methods({
 		}
 	},
 
+	/* 
+		How can I make this more secure? 
+		 	- Right its only protection is obsecurity of the token
+		 	- Users can only get the unique token through email
+			- Maybe ask them for the email account related to the key? (More secure but more steps for the user)
+			- Can call this Meteor function from the client and change
+			  the public key in the database
+		NOTE:
+			- After using token to store public_key, remove the key/pair value or set to "" or null
+			- So you can no longer access the acount with the token, for security reasons
+			- With this we could probably remove the "isPrivateKeyDownloaded" field
+	*/ 
 	savePublicKey: function(token, public_key) {
 		check([token, public_key], [String]);
 
@@ -41,31 +51,32 @@ Meteor.methods({
 			urlId: token
 		});
 
-		// Email account found
-		if (user) {
+
+		if ( user ) {
 			var updateStatus = EmailAccounts.update(
 				{ urlId: token },
 				{ $set: { publicKey: public_key } },
 				{ upsert: false }
 			);
 
-			console.log("Update Status : ", updateStatus);
-
-			if (updateStatus) {
+			if ( updateStatus ) {
+				// Able to save
 				return {
 					message: "Saved Public Key",
 					saveSuccessful: true 
 				};
 			} else {
+				// Unable to save public key using the token as the query
+				// Can't think of a use case when this happens
+				// If happens, need to set private key status back to false. Think
 				return {
 					message: "Unable to save Public Key",
 					saveSuccessful: false 
 				};
 			}
 		} else {
-			// Account not found
-			// return error
-			// Need to figure out how to handle this case. Shouldn't happen.
+			// Unable to find user account based off of the token passed in
+			// Return Error
 			return {
 				message: "Unable to find user account.",
 				saveSuccessful: false
